f9409e06a7d1a93585af2f87ac616e0aaf67937f
Iliyan Vangelov <iliyan.vangelov@gmail.com>
2018-02-04 00:10:32 +0200

    Introduce connect, reducers and LibraryList component - Move App.js to src - Add library reducer - Add json file with hardcoded data for libraries

---
+++ src/components/LibraryList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+    render() {
+    return;
+    }
+}
+
+const mapStateToProps = state => {
+    return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
